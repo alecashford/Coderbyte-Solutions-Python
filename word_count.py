9a2215b561d161c5ad0f2b1f75f5724a3b144e5a
zagadooga <ashford91@gmail.com>
2014-03-02 15:51:15 -0800

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -1,3 +1,8 @@
+#Text of challenge: "Have the function WordCount(str) take the str string parameter
+#being passed and return the number of words the string contains (ie. "Never eat
+#shredded wheat" would return 4). Words will be separated by single spaces."
+
+
 def WordCount(str):
 
     wordsplit = str.split(" ")    
