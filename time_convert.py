640c0f166417049617b5d24779948c2671b31f83
zagadooga <ashford91@gmail.com>
2014-03-02 15:49:06 -0800

    Update time_convert.py

--- time_convert.py
+++ time_convert.py
@@ -1,3 +1,9 @@
+#Have the function TimeConvert(num) take the num parameter being passed
+#and return the number of hours and minutes the parameter converts to
+#(ie. if num = 63 then the output should be 1:3). Separate the number
+#of hours and minutes with a colon."
+
+
 def TimeConvert(num): 
 
     hours = (num / 60)
