bec2fdc8ee99416d78a420af73fadc3565b1b789
zagadooga <ashford91@gmail.com>
2014-03-02 15:34:25 -0800

    Update letter_capitalize.py

--- letter_capitalize.py
+++ letter_capitalize.py
@@ -1,3 +1,8 @@
+#Text of challenge: "Have the function LetterCapitalize(str) take the
+#str parameter being passed and capitalize the first letter of each
+#word. Words will be separated by only one space."
+
+
 def LetterCapitalize(str):
     
     words = str.split(" ")
