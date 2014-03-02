db6098c9b049feb73bca4dd71dad63b1463f5144
zagadooga <ashford91@gmail.com>
2014-03-02 15:45:42 -0800

    Update prime_time.py

--- prime_time.py
+++ prime_time.py
@@ -1,3 +1,8 @@
+#Text of challenge: "Have the function PrimeTime(num) take the num parameter
+#being passed and return the string true if the parameter is a prime number,
+#otherwise return the string false. The range will be between 1 and 2^16."
+
+
 def PrimeTime(num):
     
     numrange = range(num)
