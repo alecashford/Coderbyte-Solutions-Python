9dcb9f3878d4117a3ba921a47d80b91fd60f2b58
zagadooga <ashford91@gmail.com>
2014-03-02 15:26:10 -0800

    Update alphabet_soup.py

--- alphabet_soup.py
+++ alphabet_soup.py
@@ -1,3 +1,10 @@
+#Text of challeng: "Have the function AlphabetSoup(str) take the str
+#string parameter being passed and return the string with the letters
+#in alphabetical order (ie. hello becomes ehllo). Assume numbers and
+#punctuation symbols will not be included in the string."
+
+
+
 punct = list( )
 
 def AlphabetSoup(str):
