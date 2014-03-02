da2882b5f63a70341d37767bde93b2c2b5e91ab5
zagadooga <ashford91@gmail.com>
2014-03-02 15:30:04 -0800

    Update check_nums.py

--- check_nums.py
+++ check_nums.py
@@ -1,3 +1,9 @@
+#Text of challenge: "have the function CheckNums(num1,num2) take both
+#parameters being passed and return the string true if num2 is greater
+#than num1, otherwise return the string false. If the parameter values
+#are equal to each other then return the string -1."
+
+
 def CheckNums(num1,num2): 
 
   num1 = int(num1)
