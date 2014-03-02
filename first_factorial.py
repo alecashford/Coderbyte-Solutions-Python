32b0b8960406000d20d89bf830d3d4b633ad9d7b
zagadooga <ashford91@gmail.com>
2014-03-02 15:32:58 -0800

    Update first_factorial.py

--- first_factorial.py
+++ first_factorial.py
@@ -1,3 +1,8 @@
+#Text of challenge: "Have the function FirstFactorial(num) take the num parameter being
+#passed and return the factorial of it (ie. if num = 4, return (4 * 3 * 2 * 1)).
+#For the test cases, the range will be between 1 and 18."
+
+
 def FirstReverse(str):
     str.reverse()
     revd = ''.join(str)
