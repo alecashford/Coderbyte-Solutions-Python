24639d61373e36ae8d2a9186e424786da2fcb678
zagadooga <ashford91@gmail.com>
2014-03-02 15:49:55 -0800

    Update vowel_count.py

--- vowel_count.py
+++ vowel_count.py
@@ -1,3 +1,8 @@
+#Text of challenge: "Have the function VowelCount(str) take the str string
+#parameter being passed and return the number of vowels the string contains
+#(ie. "All cows eat grass" would return 5). Do not count y as a vowel for this challenge."
+
+
 vowels = ["a", "e", "i", "o", "u"]
 
 vowellist = []
