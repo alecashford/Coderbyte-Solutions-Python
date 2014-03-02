f11b6b96954898c5be3284c7becd0929651b4c40
zagadooga <ashford91@gmail.com>
2014-03-02 15:40:24 -0800

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,8 @@
+#Text of challenge: "Have the function Palindrome(str) take the str parameter being
+#passed and return the string true if the parameter is a palindrome, (the string is
+#the same forward as it is backward) otherwise return the string false. For example:
+#"racecar" is also "racecar" backwards. Punctuation and numbers will not be part of the string."
+
 def Palindrome(str): 
 
     length = (len(str) - 1)
