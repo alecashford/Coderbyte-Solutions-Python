efb7a5142b1c4e8254f9cfa08a535f1858eb0191
zagadooga <ashford91@gmail.com>
2014-03-02 15:48:12 -0800

    Update simple_adding.py

--- simple_adding.py
+++ simple_adding.py
@@ -1,3 +1,8 @@
+#Text of challenge: "Have the function SimpleAdding(num) add up all the
+#numbers from 1 to num. For the test cases, the parameter num will be
+#any number from 1 to 1000."
+
+
 def SimpleAdding(num):
     a = range((num + 1))
     total = sum(a)    
