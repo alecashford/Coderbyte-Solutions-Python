cbffdfd2fde31f6133899703e1f61ddcd80aeac6
zagadooga <ashford91@gmail.com>
2014-03-02 15:24:39 -0800

    Update ab_check.py

--- ab_check.py
+++ ab_check.py
@@ -1,3 +1,10 @@
+#Text of challenge: "Take the str parameter being passed and return the string true
+#if the characters a and b are separated by exactly 3 places anywhere
+#in the string at least once (ie. "lane borrowed" would result in true
+#because there is exactly three characters between a and b). Otherwise
+#return the string false."
+
+
 def ABCheck(str):
 
     for i in range((len(str) - 4)):
